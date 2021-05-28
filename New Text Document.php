016fa3131ee09f85cb6462c0bb532036552edd52
Learner625 <82182525+Learner625@users.noreply.github.com>
2021-05-28 11:42:35 +0600

    Add files via upload

---
+++ New Text Document.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="jj">
+
+    my repo1
+    </div>
+    <div class="jj">
+
+    my repo2
+    </div>
+    <div class="jj">
+
+    my repo3
+    </div>
+    <div class="jj">
+
+    my repo4
+    </div>
+    <div class="jj">
+
+    my repo5
+    </div>
+    <div class="jj">
+
+    my repo6
+    </div>
+</body>
+</html>
